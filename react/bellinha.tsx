bbe2ba6bce8105d8c6b8c313c61eca452bb82661
unknown <biazzioxbox@gmail.com>
2025-06-24 15:05:02 -0300

    atualizado manual bellinha

--- react/bellinha.tsx
+++ react/bellinha.tsx
@@ -4,7 +4,7 @@ import styles from './styles.css';
 const BtDownload = () => {
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = 'https://stermax.com.br/images_idealine/manuais/Manual_-DIGITAL-IDEALCLAVE_5L-Nov-24-1.zip';
+    link.href = 'https://stermax.com.br/images_idealine/manuais/Manual Digital Idealclave Bellinha 5l - Maio 2025.zip';
     link.download = 'STM-Manual-La-Belle-8L-2024.pdf';
     document.body.appendChild(link);
     link.click();
