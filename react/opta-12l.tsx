bc08ab8b4f0b3d7ed5b42d8f0d80a3857ddb13ed
unknown <biazzioxbox@gmail.com>
2025-07-29 23:07:38 -0300

    alterado arquivos dos manuais

--- react/opta-12l.tsx
+++ react/opta-12l.tsx
@@ -4,7 +4,7 @@ import styles from './styles.css';
 const BtDownload = () => {
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = 'https://stermax.com.br/images_idealine/manuais/MANUAL-OPTA-12L_Rev-04.02.2025-compactado.zip';
+    link.href = 'https://stermax.com.br/images_idealine/manuais/MANUAL OPTA 12L_Rev 04.02.2025  1.zip';
     link.download = 'STM-Manual-La-Belle-8L-2024.pdf';
     document.body.appendChild(link);
     link.click();
--- react/vida-21l.tsx
+++ react/vida-21l.tsx
@@ -4,7 +4,7 @@ import styles from './styles.css';
 const BtDownload = () => {
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = 'https://stermax.com.br/images_idealine/manuais/MANUAL-VIDA-21L_Rev-04.02.2025-compactado.zip';
+    link.href = 'https://stermax.com.br/images_idealine/manuais/MAN.VIDA 21L - REV000_12.02.2025.zip';
     link.download = 'STM-Manual-La-Belle-8L-2024.pdf';
     document.body.appendChild(link);
     link.click();
