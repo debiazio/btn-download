0be2679fa82cb26c74109759a3e96ece1a31999e
Bruno de Biazio <biazzioxbox@gmail.com>
2026-07-29 17:21:34 -0300

    add mais um catálogo

--- react/nossas-linhas.tsx
+++ react/nossas-linhas.tsx
@@ -4,7 +4,7 @@ import styles from './styles.css';
 const BtDownload = () => {
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = 'https://stermax.com.br/images_idealine/catalogosMadu/LA_MADU_Folder-SET_Digital.zip';
+    link.href = 'https://stermax.com.br/images_idealine/catalogosMadu/456.1_AF_MAD.U_CatalogoDigital.zip';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
